aa770867537985c4a9b1ba6570036b6b53dfcf16
Matt Kleven <klevennb115@gmail.com>
2019-01-15 15:25:30 -0600

    Got header working

---
+++ src/Components/FamousPerson.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class FamousPerson extends Component{
+    render(){
+        return(
+            
+        )
+    }
+}
+
+export default FamousPerson;
---
+++ src/Components/header.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Header extends Component{
+    render(){
+        return(
+            <div class="App-header">
+                <header>Famous People</header> 
+            </div>
+        )
+    }
+}
+
+
+export default Header;
